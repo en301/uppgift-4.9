09bdbade0f671f56ca59869993a3eecb11145150
en301 <emilsjostrand1000@gmail.com>
2023-10-20 12:03:31 +0200

    Add project files.

---
+++ uppgift 4.9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift_4_9
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in hur många stjärnor som kateten ska vara.");
+            int tal = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tal; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
